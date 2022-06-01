5bfbdc209099509ab459c24641dce90d1d802c2e
Jonatas Paz <62933084+jonataspaz@users.noreply.github.com>
2022-06-01 23:13:51 +1200

    Start Countdown build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import CountdonwTimer from './Components/CountdownTimer/CountdownTimer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My Countdonw Timer
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CountdonwTimer/>
     </div>
   );
 }
---
+++ src/Components/CountdownTimer/CountdownTimer.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import './Countdowntimer.css';
+
+const defaultRemainingTime = {
+    seconds: '00',
+    minutes: '00',
+    hours: '00',
+    days: '00'
+}
+
+const CountdonwTimer = () => {
+    const [remainingTime, setRemainingTimer] = useState(defaultRemainingTime);
+
+    useEffect(() => {
+        setInterval(() => {
+
+        }, 1000);
+    },[])
+
+    function updateRemainingTime() {
+
+    }
+
+    return(
+        <div className="countdown-timer">
+            <span>{remainingTime.days}</span>
+            <span>days</span>
+            <span>{remainingTime.hours}</span>
+            <span>hours</span>
+            <span>{remainingTime.minutes}</span>
+            <span>minutes</span>
+            <span>{remainingTime.seconds}</span>
+            <span>seconds</span>
+        </div>
+    );
+}
+
+export default CountdonwTimer;
